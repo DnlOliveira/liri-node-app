dcf1a79d8e4e7372b8f19aea3bd5c0c85d1e6ddc
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-02-14 12:03:17 -0500

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'oYBvs4iwYvyzTjfRWkFPUAkAH',
+  consumer_secret: 'xPcne0OR1RqYxwXEYULwmXuHcIXu9MJfv5KE7ooh7sRcFYQGRd',
+  access_token_key: '2428009524-y8RLyxKrmBW1xAF1mJIsaTCnfteYnAYyANlPAic',
+  access_token_secret: 'rmTnXBaOLjP7VcuOy7HWbGFEM5GWytuol90KAwQyIdgr6',
+}
